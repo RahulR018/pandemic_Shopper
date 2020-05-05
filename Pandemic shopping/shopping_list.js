26ee2efc1efa5056c5d84b6cbb94724da38aa0f1
RahulR018 <rahulrmenon@live.com>
2020-05-05 23:46:10 +0530

    first attempt

---
+++ Pandemic shopping/shopping_list.js
@@ -0,0 +1,30 @@
+function grab() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            console.log(response);
+            var shopping_list = response.shopping_list;
+            console.log(shopping_list[0]['Serial number']);
+            var header = [];
+            var j = 0;
+            for (i in shopping_list[0]) {
+                header[j] = i;
+                j = j + 1;
+            }
+            console.log(header);
+            console.log(shopping_list.length);
+            var q = 0;
+            for (a = 0; a < 10; a++) {
+                for (var i in [0, 1, 2, 3, 4, 5]) {
+                    document.getElementsByTagName('td')[q].innerHTML = shopping_list[a][header[i]];
+                    q = q + 1;
+                }
+            }
+        }
+    }
+    xhttp.open("GET", "shopping_list.json", true);
+    xhttp.send();
+}
